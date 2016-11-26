a2a398516882ec0ea9951218c4a3ab0d4f023a7f
oanaciocioman <oana.ciocioman@gmail.com>
2016-11-26 21:02:36 +0000

    First commit

---
+++ src/EchoFilterTest.java
@@ -0,0 +1,50 @@
+
+/*
+DEVELOPING GAME IN JAVA
+
+Caracteristiques
+
+Editeur : NEW RIDERS
+Auteur : BRACKEEN
+Parution : 09 2003
+Pages : 972
+Isbn : 1-59273-005-1
+Reliure : Paperback
+Disponibilite : Disponible a la librairie
+*/
+
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+/**
+ * An example of playing a sound with an echo filter.
+ *
+ * @see EchoFilter
+ * @see SimpleSoundPlayer
+ */
+public class EchoFilterTest {
+
+    public static void main(String[] args) {
+
+        // load the sound
+        SimpleSoundPlayer sound = new SimpleSoundPlayer("../sounds/voice.wav");
+
+        // create the sound stream
+        InputStream is = new ByteArrayInputStream(sound.getSamples());
+
+        // create an echo with a 11025-sample buffer
+        // (1/4 sec for 44100Hz sound) and a 60% decay
+        EchoFilter filter = new EchoFilter(11025, .6f);
+
+        // create the filtered sound stream
+        is = new FilteredSoundStream(is, filter);
+
+        // play the sound
+        sound.play(is);
+
+        // due to bug in Java Sound, explicitly exit the VM.
+        System.exit(0);
+    }
+
+}
